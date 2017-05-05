c856537a90cd4710e3633f995e8458821c4fc7ba
Ileha <i.leha98@mail.ru>
2017-05-05 21:57:45 +0300

    comment MyRandom

--- Assets/MyRandom.cs
+++ Assets/MyRandom.cs
@@ -13,15 +13,15 @@ namespace Assets
                 rnd = new Random();
             }
 
-            public static double GetRandomDouble()
+            public static double GetRandomDouble() //вернёт случайное число в диапазое 0 - 1
             {
                 return rnd.NextDouble();
             }
-            public static double GetRandomDouble(double max)
+            public static double GetRandomDouble(double max) //вернёт случайное число в диапазое 0 - max
             {
                 return GetRandomDouble() * max;
             }
-            public static double GetRandomDouble(double min, double max)
+            public static double GetRandomDouble(double min, double max) //вернёт случайное число в диапазое min - max
             {
                 return rnd.NextDouble() * (max - min) + min;
             }
