241262542e5444038949bb9206a2e0639ebe74af
Ileha <i.leha98@mail.ru>
2017-05-08 19:13:43 +0300

    Deleted the unnecessary delegate

--- Control/Delegates.cs
+++ Control/Delegates.cs
@@ -3,6 +3,5 @@
     public delegate Iindividual Crossover(Iindividual parent1, Iindividual parent2);
     public delegate void Mutator(Iindividual target);
 	public delegate Iindividual CreateNewIindividual();
-	public delegate Iindividual CreateNewIindividualEmpty();
 	public delegate Iindividual CopyFromIindividual(Iindividual _individual);
 }
