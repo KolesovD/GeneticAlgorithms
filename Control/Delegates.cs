4cf3e2770ed213b2c694a5eaf67f34d946169625
Ileha <i.leha98@mail.ru>
2017-05-08 19:10:05 +0300

    Add new delegate for create empty Iindividual

--- Control/Delegates.cs
+++ Control/Delegates.cs
@@ -3,5 +3,6 @@
     public delegate Iindividual Crossover(Iindividual parent1, Iindividual parent2);
     public delegate void Mutator(Iindividual target);
 	public delegate Iindividual CreateNewIindividual();
+	public delegate Iindividual CreateNewIindividualEmpty();
 	public delegate Iindividual CopyFromIindividual(Iindividual _individual);
 }
