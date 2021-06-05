6fbbc49f138544cc644932888f2d84f7346258c5
Ileha <i.leha98@mail.ru>
2021-06-05 22:29:45 +0300

    +tests

---
+++ UnitTests/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void IndexTest()
+        {
+            // Arrange
+            int a = 5;
+
+            // Act
+            a += 10;
+
+            // Assert
+            Assert.Equal(15, a);
+            //Assert.NotNull(result);
+            //Assert.Equal("Index", result?.ViewName);
+        }
+    }
+}
